f3142545d6e9a0e836ab0b46a4f001ae30a29b1e
SpiriTVoiDD <69366493+SpiriTVoiDD@users.noreply.github.com>
2020-08-31 19:15:33 +0200

    Edited: - Movie: Filled in the class body to fit the API, TODO finished.

--- app/src/main/java/com/useless/boosterapp4/network/Movie.kt
+++ app/src/main/java/com/useless/boosterapp4/network/Movie.kt
@@ -1,5 +1,36 @@
 package com.useless.boosterapp4.network
 
-    class Movie {
-        //TODO Fill class with attributes from the API documentation online.
+import com.google.gson.annotations.SerializedName
+
+//Re-check if we need anything else from Movie from the API, we already got the bare minimum for the design.
+
+class Movie(
+        @SerializedName("id")
+        val id: Int,
+        @SerializedName("poster_path")
+        val posterPath: String?,
+        @SerializedName("original_language")
+        val lang: String,
+        @SerializedName("original_title")
+        val title: String,
+        @SerializedName("release_date")
+        val date: String,
+        @SerializedName("runtime")
+        val runtime: Int?,
+        @SerializedName("vote_average")
+        val voteAvg: Number,
+        @SerializedName("vote_count")
+        val voteCnt: Int,
+        @SerializedName("genres")
+        val genres: List<Genre>
+        ) {
+
+
+        data class Genre(
+            @SerializedName("id")
+            val id: Int,
+            @SerializedName("name")
+            val name: String
+        )
+
 }
