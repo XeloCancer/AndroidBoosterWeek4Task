546aa9db2f93fa718ae853dbc7ad70b1dc772f5d
SpiriTVoiDD <69366493+SpiriTVoiDD@users.noreply.github.com>
2020-08-31 19:30:25 +0200

    Added: - MovieList: Class to hold List<Movie>, total pages, and current page. - Two preliminary previous and next page methods

--- app/src/main/java/com/useless/boosterapp4/MainActivity.kt
+++ app/src/main/java/com/useless/boosterapp4/MainActivity.kt
@@ -2,10 +2,30 @@ package com.useless.boosterapp4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+    fun nextPage(pageNum: Int, totalPages: Int){
+        if(pageNum == totalPages){
+            Toast.makeText(applicationContext, "You're already at the last page", Toast.LENGTH_SHORT).show()
+            return
+        }else{
+            //TODO: call API with same data but query page = pageNum + 1
+        }
+    }
+    fun prevPage(pageNum: Int){
+        if(pageNum == 1){
+            Toast.makeText(applicationContext, "You're already at the first page", Toast.LENGTH_SHORT).show()
+            return
+        }else{
+            //TODO: call API with same data but query page = pageNum - 1
+        }
     }
 }
