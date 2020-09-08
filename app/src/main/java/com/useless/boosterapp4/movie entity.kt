fad2c40b72aa264462482c4fdf7304b671625e86
Lenovo <meroyehia66@gmail.com>
2020-09-08 20:21:03 +0200

    entites are created

---
+++ app/src/main/java/com/useless/boosterapp4/movie entity.kt
@@ -0,0 +1,26 @@
+package com.useless.boosterapp4
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity
+data class `movieentity` (
+    @PrimaryKey
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("poster_path")
+    val posterPath: String?,
+    @SerializedName("original_language")
+    val lang: String,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("release_date")
+    val date: String,
+    @SerializedName("vote_average")
+    val voteAvg: Number,
+    @SerializedName("vote_count")
+    val voteCnt: Int,
+    @SerializedName("overview")
+    val overview : String,
+)
+
