0214001ff12897c70c3414eca538f95072e44b92
Lenovo <meroyehia66@gmail.com>
2020-09-09 12:01:26 +0200

    required dao objects are created

--- app/src/main/java/com/useless/boosterapp4/moviedao.kt
+++ app/src/main/java/com/useless/boosterapp4/moviedao.kt
@@ -1,4 +1,19 @@
 package com.useless.boosterapp4
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+@Dao
 interface moviedao {
+    @Insert
+    fun addmovie(movie:movieentity)
+    @Query("SELECT*FROM movie")
+    fun getmovie():movieentity
+    @Delete
+    fun deletemovie(movie:movieentity)
+    @Query("DELETE FROM movie")
+    fun deleteAll()
+    @Query("SELECT*FROM movie")
+    fun getallmovies():List<movieentity>
 }
--- app/src/main/java/com/useless/boosterapp4/movielistdao.kt
+++ app/src/main/java/com/useless/boosterapp4/movielistdao.kt
@@ -1,4 +1,18 @@
 package com.useless.boosterapp4
-
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+@Dao
 interface movielistdao {
+    @Insert
+    fun addmovielist(movie:movielistentity)
+    @Query("SELECT*FROM movielist")
+    fun getmovielist():movielistentity
+    @Delete
+    fun deletemovielist(movie:movielistentity)
+    @Query("DELETE FROM movielist")
+    fun deleteAll()
+    @Query("SELECT*FROM movielist")
+    fun getallmovielists():List<movielistentity>
 }
