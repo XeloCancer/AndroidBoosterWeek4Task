e6d381e9c435bb1bc12d1e915877fb8ebbd7b0a3
SpiriTVoiDD <69366493+SpiriTVoiDD@users.noreply.github.com>
2020-09-13 03:51:57 +0200

    Added kotlin-kapt plugin

--- app/src/main/java/com/useless/boosterapp4/network/APIInterface.kt
+++ app/src/main/java/com/useless/boosterapp4/network/APIInterface.kt
@@ -9,11 +9,15 @@ interface ApiInterface {
 
     //Gets a list of popular movies, queries: apiKey, Page number, Language
     @GET("movie/popular")
-    fun doGetMoviesList(@Query("api_key") apiKey: String, @Query("language") lang: String = "en-US", @Query("page") page: Int = 1): Call<MovieList>
+    fun doGetMoviesList(@Query("api_key") apiKey: String,
+                        @Query("language") lang: String = "en-US",
+                        @Query("page") page: Int = 1): Call<MovieList>
 
     @GET("movie/{id}")
-    fun doGetMovieByID(@Path("id") movieID: Int, @Query("api_key") apiKey: String): Call<MovieResponse>
+    fun doGetMovieByID(@Path("id") movieID: Int,
+                       @Query("api_key") apiKey: String): Call<MovieResponse>
 
     @GET("movie/top_rated")
-    fun doGetMovieByRate(@Query("api_key")apiKey: String, @Query("page") page: Int = 1): Call<MovieList>
+    fun doGetMovieByRate(@Query("api_key")apiKey: String,
+                         @Query("page") page: Int = 1): Call<MovieList>
 }
