bcfc8187c92a24b70d9d3846a161391a0a501247
XenoLancer <modogogo2000@hotmail.com>
2020-09-30 16:10:47 +0200

    Keys file is now removed from github

--- app/src/main/java/com/useless/boosterapp4/data/keys/APIKeys.kt
+++
@@ -1,6 +0,0 @@
-package com.useless.boosterapp4.data.keys
-
-object APIKeys {
-    const val googleAPIKey = "AIzaSyDsVxqRYXU2KrpxHvMUQ1uZjVPT2vPp6ac"
-    const val tmdbAPIKey = "6637f7d017283c784ff6746c01f71453"
-}
